7f6d67816144172cdd42b624a3cdd894964d95f2
Brian Dawes <bad2140@columbia.edu>
2014-09-18 15:09:31 -0400

    problem2a.py
    Added comments, changed plotting a bit, rewrote L2 norm using vector operations, imported setD instea of copying it in

--- projects/ProblemSet2/problem2a.py
+++ projects/ProblemSet2/problem2a.py
@@ -1,124 +1,73 @@
 # -*- coding: utf-8 -*-
 """
 Created on Wed Sep 17 22:29:42 2014
-
+Script to solve problem 2a on problem set 2.
+Calculates and plots the errors in the 1st and 2nd derivatives of f=x^2+sin4pix
+over the interval [0,1] as a function of the mesh width.
 @author: tfuser
 """
 
 import scipy.sparse as sp
 import numpy as np
 import pylab
-import math
-import matplotlib.pyplot as plt
 
-from fdcoeffF import fdcoeffF
-
-def setD(k,x):
-    """ 
-    example function for setting k'th order sparse differentiation matrix over 
-    arbitrary mesh x with a 3 point stencil
-    
-    input:
-        k = degree of derivative <= 2
-        x = numpy array of coordinates >=3 in length
-    returns:
-        D sparse differention matrix
-    """
-    
-    assert(k < 3) # check to make sure k < 3
-    assert(len(x) > 2)
-    
-    N = len(x)
-    # initialize a sparse NxN matrix in "lil" (linked list) format
-    D = sp.lil_matrix((N,N))
-    # assign the one-sided k'th derivative at x[0]
-    D[0,0:3] = fdcoeffF(k,x[0],x[0:3])
-    # assign centered k'th ordered derivatives in the interior
-    for i in xrange(1,N-1):
-        D[i,i-1:i+2] = fdcoeffF(k,x[i],x[i-1:i+2])
-    # assign one sided k'th derivative at end point x[-1]
-    D[N-1,-3:] = fdcoeffF(k,x[N-1],x[-3:])
-    
-    # convert to csr (compressed row storage) and return
-    return D.tocsr()
-
-# quicky test program
-def plotDf(x,f,title=None):
-    """ Quick test routine to display derivative matrices and plot
-        derivatives of an arbitrary function f(x)
-        input: x: numpy array of mesh-points
-               f: function pointer to function to differentiate
-    """  
-    # calculate first and second derivative matrices
-    D1 = setD(1,x) 
-    D2 = setD(2,x)
-    
-    print D1
-    print D2
-    
-    # show sparsity pattern of D1
-    pylab.figure()
-    pylab.spy(D1,precision=1.e-6)
-    pylab.title("Sparsity pattern of D1")
-        
-    # plot a function and it's derivatives
-    y = f(x)    
-    pylab.figure()
-    pylab.plot(x,y,x,D1*y,x,D2*y,x,2*x+4*math.pi*cos(4*math.pi*x))
-    pylab.legend(['f','D1*f','D2*f'],loc="best")
-    if title:
-        pylab.title(title)
-    pylab.show(block=False)
+from diffMatrix import setD
     
 def calcError(f,N,k,DkF):
+    '''
+    Method to calculate the absolute mesh error in the kth derivative over 
+    a uniform mesh from 0 to 1 containing N points.
+    inputs:
+        f - input function
+        N - number of points in mesh
+        k - order of derivative
+        DkF - kth derivative of f
+    '''
     x = np.linspace(0,1,N+1)
     h = 1.0/N
     
     Dk = setD(k,x)
-    return sqrt(h)*normL2(Dk*f(x)-DkF(x))
     
-def normL2(x):
-    norm = 0    
-    for i in range(len(x)):
-        norm += x[i]**2
-    return sqrt(norm)        
+    return np.sqrt(h*np.sum((Dk*f(x)-DkF(x))**2))   
 
 def main():
+    # Test function and first and second derivatives
     def f(x):
-        return x**2 + sin(4*pi*x)
+        return x**2 + np.sin(4*np.pi*x)
     
     def d1f(x):
-        return 2*x+4*pi*cos(4*pi*x)
+        return 2*x+4*np.pi*np.cos(4*np.pi*x)
         
     def d2f(x):
-        return 2-16*pi**2*sin(4*pi*x)
-        
-    #plotDf(x,f,"f=x^2 + sin(4\pi x)")
-
+        return 2-16*np.pi**2*np.sin(4*np.pi*x)
+    
+    # Number of points in mesh    
     N = [8,16,32,64,128,256,512,1024]
 
+    # Error in first and second derivatives for each N
     eh1 = []
-    eh2 = []   
+    eh2 = []  
+    # Mesh width for each N
     h = []
+    
+    # Calc errors and h
     for n in N:
         eh1.append(calcError(f,n,1,d1f))
         eh2.append(calcError(f,n,2,d2f))     
         h.append(1.0/n)
     
-    print h
-    print eh2
-    
-    pylab.figure()
-    pylab.axis([0.0001,1,0.0001,10])
-    pylab.loglog(h,eh1,'-o')
-    pylab.legend(['$e_{h}$'],loc="best")
-    pylab.title('Error in first derivative')
-    
-    pylab.figure()
-    pylab.axis([0.0001,1,.01,100])    
-    pylab.loglog(h,eh2,'-o')
-    pylab.legend(['$e_{h}$'],loc='best')
-    pylab.title("Error in second derivative")
+    # Plot error in derivatives
+    pylab.figure(1)
+    pylab.axis([0.0008,.2,0.0001,100])
+    pylab.loglog(h,eh1,'-o',h,eh2,'-d')
+    pylab.xlabel('Mesh width')
+    pylab.ylabel('Absolute mesh error')
+    pylab.title('Error in derivatives of $f=x^2+\sin(4\pi x)$')
+    pylab.legend(['First derivative','Second derivative'],loc='best')
+    pylab.grid()
+    pylab.draw()
+    pylab.show()
+
 
 if __name__ == "__main__":
     main()
