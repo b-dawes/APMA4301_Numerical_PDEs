Completed error calculation/graphing. Made small fix to matrix construction in poisson2d_nonuniformMesh.py and poisson2d_neumann.py. Unfortunately neumann.py still doesn't work.

ProblemSets/ProblemSet03/poisson2d_mms.py
@@ -28,7 +28,6 @@ def grid_norm2(f,h):
 def calcSolution(m,show_matrix,show_result):
     a = 0.0
     b = 1.0
-    m = 16                   # number of interior points in each direction
     h = (b-a)/(m+1)
     x = np.linspace(a,b,m+2)   # grid points x including boundaries
     y = np.linspace(a,b,m+2)   # grid points y including boundaries
@@ -84,7 +83,7 @@ def calcSolution(m,show_matrix,show_result):
     usoln[1:-1, 1:-1] = uvec.reshape( (m,m) )
     
     # Find errors
-    abs_err = grid_norm2(usoln-u_exact(x,y),h)
+    abs_err = grid_norm2(usoln-u_exact(X,Y),h)
     rel_err = abs_err/grid_norm2(usoln,h)
     print "m = {0}".format(m)
     print "Absolute error = {0:10.3e}, relative error = {1:10.3e}".format(abs_err,rel_err)
@@ -123,6 +122,7 @@ def main():
     pylab.title('Errors in Solving a Poisson Problem')
     pylab.xlabel('Mesh Width')
     pylab.ylabel('Error')
+    pylab.grid()
     pylab.legend(['Absolute Error','Relative Error'],loc='best')
 
 if __name__ == "__main__":

ProblemSets/ProblemSet03/poisson2d_neumann.py
@@ -48,9 +48,9 @@ def calcSolution(ax,bx,ay,by,mx,my,show_matrix,show_result):
     usoln[0,:] = u_exact(ax,y)
 
     # adjust the rhs to include boundary terms: 
-    rhs[:,0] -= usoln[1:,0] / hx**2
-    rhs[:,-1] -= usoln[1:,-1] / hx**2
-    rhs[0,:] -= usoln[0,1:-1] / hy**2
+    rhs[:,0] -= usoln[1:,0] / hy**2
+    rhs[:,-1] -= usoln[1:,-1] / hy**2
+    rhs[0,:] -= usoln[0,1:-1] / hx**2
     # include Neumann term on boundary
     rhs[-1,:] -= 2*u_exact(bx,y[1:-1])/hx
 
@@ -64,8 +64,9 @@ def calcSolution(ax,bx,ay,by,mx,my,show_matrix,show_result):
     exNeumann = ex   # Change last part of bottom diagonal for Neumann conditions
     exNeumann[len(ex)-1] = 2
     ey = np.ones(my)
-    T = sp.spdiags([1/alpha*ey,-2*(alpha+1/alpha)*ey,1/alpha*ey],[-1,0,1],my,my)
-    S = sp.spdiags([alpha*exNeumann,alpha*ex],[-1,1],mx+1,mx+1)
+    T = sp.spdiags([alpha*ey,-2*(alpha+1/alpha)*ey,alpha*ey],[-1,0,1],my,my)
+    S = sp.spdiags([1/alpha*exNeumann,1/alpha*ex],[-1,1],mx+1,mx+1)
+    print S
     A = (sp.kron(Ix,T) + sp.kron(S,Iy)) / (hx*hy)    
     A = A.tocsr()
     

ProblemSets/ProblemSet03/poisson2d_nonuniformMesh.py
@@ -50,10 +50,10 @@ def calcSolution(ax,bx,ay,by,mx,my,show_matrix,show_result):
     usoln[-1,:] = u_exact(bx,y)
 
     # adjust the rhs to include boundary terms: 
-    rhs[:,0] -= usoln[1:-1,0] / hx**2
-    rhs[:,-1] -= usoln[1:-1,-1] / hx**2
-    rhs[0,:] -= usoln[0,1:-1] / hy**2
-    rhs[-1,:] -= usoln[-1,1:-1] / hy**2
+    rhs[:,0] -= usoln[1:-1,0] / hy**2
+    rhs[:,-1] -= usoln[1:-1,-1] / hy**2
+    rhs[0,:] -= usoln[0,1:-1] / hx**2
+    rhs[-1,:] -= usoln[-1,1:-1] / hx**2
 
 
     # convert the 2d grid function rhs into a column vector for rhs of system:
@@ -64,8 +64,8 @@ def calcSolution(ax,bx,ay,by,mx,my,show_matrix,show_result):
     Iy = sp.eye(my,my)
     ex = np.ones(mx)
     ey = np.ones(my)
-    T = sp.spdiags([1/alpha*ey,-2*(alpha+1/alpha)*ey,1/alpha*ey],[-1,0,1],my,my)
-    S = sp.spdiags([alpha*ex,alpha*ex],[-1,1],mx,mx)
+    T = sp.spdiags([alpha*ey,-2*(alpha+1/alpha)*ey,alpha*ey],[-1,0,1],my,my)
+    S = sp.spdiags([1/alpha*ex,1/alpha*ex],[-1,1],mx,mx)
     A = (sp.kron(Ix,T) + sp.kron(S,Iy)) / (hx*hy)    
     A = A.tocsr()
     